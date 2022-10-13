b5ef7c41ae3d80a41ee21bc8ebe4e73d9c3e6fe6
Леонид <Fear-1995@mail.ru>
2022-10-13 15:33:14 +0300

    add get test

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -8,5 +8,10 @@ class TestCreateLocation:
 
     def test_create_new_place(self):
 
-        print("Метод POST")
-        result_post = Response = GoogleMapsAPI.create_new_location()
+        print("\nМетод POST")
+        result_post : Response = GoogleMapsAPI.create_new_location()
+        check_post = result_post.json()
+        place_id = check_post.get('place_id')
+
+        print("\nМетод GET")
+        result_get : Response = GoogleMapsAPI.get_new_location(place_id)
--- utils/api.py
+++ utils/api.py
@@ -35,3 +35,16 @@ class GoogleMapsAPI:
         result_post = Http_methods.post(post_url, json_for_create_new_location)
         print(result_post.text)
         return result_post
+
+
+    """ Метод для проверки новой локации """
+
+    @staticmethod
+    def get_new_location(place_id):
+
+        get_resurce='/maps/api/place/get/json'  # Ресурс метода GET
+        get_url = base_url + get_resurce + key + "&place_id=" + place_id
+        print(get_url)
+        result_get = Http_methods.get(get_url)
+        print(result_get.text)
+        return result_get
